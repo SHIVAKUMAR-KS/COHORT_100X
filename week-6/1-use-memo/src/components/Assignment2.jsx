23d79d25e377c006927a6f0475954305ce94bffd
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-02-04 16:28:59 +0530

    Assignment done for week 3 6

--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
+// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items.
+
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
+
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
 
 const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
